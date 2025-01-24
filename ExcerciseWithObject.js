03b7289356bc1c8830c784baa390c9be9dff3b54
atamayo1 <anthony1_144@hotmail.com>
2025-01-24 12:12:01 -0500

    i added the functionality to delete one item of the object

--- ExcerciseWithObject.js
+++ ExcerciseWithObject.js
@@ -7,3 +7,19 @@ let persona = {
 persona.habilidad = "Frontend Developer";
 
 console.log(persona);
+
+persona.ubicacion = {
+    codigoPostal: 7600,
+    ciudad: "Cali",
+    calle: "60B",
+    numero: "107"
+};
+
+function DeleteFirstItem(item, keyToDelete) {
+    delete item[keyToDelete];
+    return item;
+}
+
+let newObject = DeleteFirstItem(persona, "edad");
+
+console.log(newObject);
