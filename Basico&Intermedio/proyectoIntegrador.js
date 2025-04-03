db8caed488cf2be0ff3dbe537ea7b332a8739519
atamayo1 <anthony1_144@hotmail.com>
2025-04-03 00:24:27 -0500

    i finish the project

--- Basico&Intermedio/proyectoIntegrador.js
+++ Basico&Intermedio/proyectoIntegrador.js
@@ -30,12 +30,16 @@ function agregarTarea(nameRequest, limitDateRequest = null) {
 }
 
 // Eliminar tarea
-function eliminarTarea(tarea) {
-    if (tareas.length > 0) {
-        tareas.splice(tareas.indexOf(tarea), 1);
+function eliminarTarea(nombreTarea) {
+    let index = tareas.findIndex(t => t.name === nombreTarea);
+    if (index !== -1) {
+        tareas.splice(index, 1);
         console.log("Tarea eliminada correctamente");
+    } else {
+        console.log("No se encontró la tarea");
     }
 }
+
 
 // Completar tarea
 function completarTarea(indice) {
@@ -71,12 +75,58 @@ function contarTareasCompletadasPorCategoria(numeroCategoria) {
 }
 
 function mostrarTareasNoCompletadas() {
-    console.log("Tareas no completadas: ");
     tareas.forEach(function (tarea) {
         if (!tarea.complete) {
-            console.log("- Nombre: " + tarea.nombre + ", Categoria: " + categoriasNombres[tarea.categoria]);
+            console.log(tarea);
         }
-    })
+    });
+}
+
+function ordenarTareasPorNombre() {
+    let total = tareas.length;
+
+    for (let j = 0; j < total; j++) {
+        for (let i = 0; i < total - 1; i++) {
+            if (tareas[i].name > tareas[i + 1].name) {
+                let temp = tareas[i];
+                tareas[i] = tareas[i + 1];
+                tareas[i + 1] = temp;
+            }
+        }
+    }
+}
+
+
+function ordenarTareasPorFechaLimite() {
+    let total = tareas.length;
+
+    for (let j = 0; j < total; j++) {
+        for (let i = 0; i < total - 1; i++) {
+            if (tareas[i].limitDate > tareas[i + 1].limitDate) {
+                let temp = tareas[i];
+                tareas[i] = tareas[i + 1];
+                tareas[i + 1] = temp;
+            }
+        }
+    }
+}
+
+function buscarTareaPorNombre(nombreTarea) {
+    let inicio = 0;
+    let fin = tareas.length - 1;
+
+    while (inicio <= fin) {
+        let posElementoMedio = Math.round((inicio + fin) / 2);
+
+        if (tareas[posElementoMedio].name === nombreTarea) {
+            return posElementoMedio;
+        } else if (tareas[posElementoMedio].name < nombreTarea) {
+            inicio = posElementoMedio + 1;
+        } else {
+            fin = posElementoMedio - 1;
+        }
+    }
+    return -1;
 }
 
 function mostrarMenu() {
@@ -91,6 +141,9 @@ function mostrarMenu() {
     console.log("8. Filtrar tareas por categoria");
     console.log("9. Visualizar cantidad de tareas completadas por categoria");
     console.log("10. Visualizar todas las tareas no completadas");
+    console.log("11. Ordenar todas las tareas alfabeticamente");
+    console.log("12. Ordenar todas las tareas por fecha limite");
+    console.log("13. Buscar una tarea por su nombre y obtener como respuesta su ubicacion");
     console.log("0. Salir");
 }
 
@@ -145,8 +198,8 @@ function interactuarConUsuario() {
                 }
                 break;
             case 5:
-                console.log(" -- Lista de tareas -- ")
-                console.log(tareas)
+                console.log(" -- Lista de tareas -- ");
+                mostrarTareasNoCompletadas();
                 break;
             case 6:
                 mostrarTodasLasCategorias();
@@ -162,6 +215,7 @@ function interactuarConUsuario() {
                     let tareasFiltradas = filtrarTareasPorCategoria(numeroCategoria);
                     console.log("Tareas filtradas por categoria " + categoriasNombres[numeroCategoria] + ": ", tareasFiltradas);
                 }
+                break;
             case 9:
                 mostrarTodasLasCategorias();
                 let numCategory = parseInt(prompt("Ingrese el numero de la categoría a visualizar: "));
@@ -171,7 +225,28 @@ function interactuarConUsuario() {
                 }
                 break;
             case 10:
+                console.log("Tareas no completadas: ");
                 mostrarTareasNoCompletadas();
+                break;
+            case 11:
+                ordenarTareasPorNombre();
+                console.log("Tareas por nombre: ");
+                console.log(tareas);
+                break;
+            case 12:
+                ordenarTareasPorFechaLimite();
+                console.log("Tareas por fecha limite: ");
+                console.log(tareas);
+                break;
+            case 13:
+                ordenarTareasPorNombre();
+                let nombreABuscar = prompt("Ingrese el nombre de la tarea a buscar: ");
+                let indiceTarea = buscarTareaPorNombre(nombreABuscar);
+                if (indiceTarea !== -1) {
+                    console.log("Tarea encontrada en el indice: ", indiceTarea);
+                } else {
+                    console.log("Tarea no encontrada");
+                }
                 break;
             default:
                 break;
